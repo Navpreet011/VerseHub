52aedb9aea05860a1a8c00b7013c7500f867d1f9
Navpreet Kaur <npreetkaur456@gmail.com>
2024-11-08 10:18:11 +0530

    save3

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import {useDispatch} from 'react-redux'
 import {logout} from '../../store/authSlice'
 import authservice from '../../appwrite/auth'
+import { useNavigate } from 'react-router-dom';
 
 export default function  LogoutBtn() {
     const dispatch=useDispatch()
+    const navigate = useNavigate();
     const LogoutHandler = () => {
-        authservice.logout()
+        authservice.logout() 
         .then(()=> {
             dispatch(logout())
+            navigate('/login');  
         })
     }
     return(
